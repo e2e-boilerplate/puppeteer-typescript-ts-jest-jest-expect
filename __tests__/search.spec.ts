1689a67d07dd7d9066c597a476ff5892191633d3
Girma Nigusse <xgirma@gmail.com>
2019-10-26 23:48:03 -0400

    cleanup

--- __tests__/search.spec.ts
+++ __tests__/search.spec.ts
@@ -2,12 +2,23 @@ import puppeteer from "puppeteer";
 
 let page: any;
 let browser: any;
+const searchBox: string = ".gLFyf.gsfi";
 
 describe("google search", () => {
   beforeAll(async () => {
     browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
-    await page.goto("https://www.google.com", { waitUntil: "networkidle0" });
+
+    await Promise.race([
+      page
+        .goto("https://www.google.com", { waitUntil: "networkidle0" })
+        .catch(() => {
+          throw new Error("Error");
+        }),
+      page.waitFor("body", { timeout: 6000 }).catch(() => {
+        throw new Error("Error");
+      })
+    ]);
   });
 
   afterAll(() => {
@@ -17,19 +28,22 @@ describe("google search", () => {
   });
 
   test("should be on google search page", async () => {
+    await page.waitFor(searchBox);
+
     const title = await page.title();
     expect(title).toEqual("Google");
   });
 
   test("should search for Cheese!", async () => {
-    const searchBox = ".gLFyf.gsfi";
     expect(!!(await page.$(searchBox))).toBe(true);
+
     await page.type(searchBox, "Cheese!", { delay: 100 });
     await page.keyboard.press("\n");
   });
 
   test('the page title should start with "Cheese!', async () => {
     await page.waitFor("#resultStats");
+
     const title = await page.title();
     const words = title.split(" ");
     expect(words[0]).toEqual("Cheese!");
