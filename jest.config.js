9e4fa48ad98ebbeb73d805e826d68058787febed
Girma Nigusse <xgirma@gmail.com>
2019-10-30 22:25:36 -0400

    init

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
   testPathIgnorePatterns: ["/node_modules/"],
   testTimeout: 10000,
-
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   }
