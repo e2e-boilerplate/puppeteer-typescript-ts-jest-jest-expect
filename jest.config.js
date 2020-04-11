8e91242fd8d62ce9c46a5b8af44d939913aabc2d
Girma Nigusse <xgirma@gmail.com>
2020-04-11 11:22:31 -0400

    updates

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
   testPathIgnorePatterns: ["/node_modules/"],
   setupFilesAfterEnv: ["./jest.setup.js"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
 };
